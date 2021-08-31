094f4b9f92c991f16299264edc4dc361e389beb1
Luke Murphey <Luke.Murphey@gmail.com>
2021-08-31 10:19:39 -0700

    Adding persistence layer

---
+++ hebrew_quiz/src/components/Persistence/index.js
@@ -0,0 +1,89 @@
+const QUIZ_STATUSES_NAME = "quizStatuses";
+
+export function storageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = "__storage_test__";
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      // everything except Firefox
+      (e.code === 22 ||
+        // Firefox
+        e.code === 1014 ||
+        // test name field too, because code might not be present
+        // everything except Firefox
+        e.name === "QuotaExceededError" ||
+        // Firefox
+        e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage &&
+      storage.length !== 0
+    );
+  }
+}
+
+export function getQuizStatus(quizID, storageOverride = null) {
+  const quizStatuses = getQuizStatuses(storageOverride);
+
+  if (quizID in quizStatuses) {
+    return quizStatuses[quizID];
+  }
+
+  return null;
+}
+
+export function getQuizStatuses(storageOverride = null) {
+  const storage = storageOverride || localStorage;
+
+  // if (storageAvailable("localStorage")) {
+  const quizStatuses = storage.getItem(QUIZ_STATUSES_NAME);
+
+  if (quizStatuses) {
+    try {
+      const quizStatusesParsed = JSON.parse(quizStatuses);
+
+      return quizStatusesParsed;
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.warn("The list of quiz statuses could not be loaded");
+      return null; // The list could not be loaded
+    }
+  }
+
+  return null;
+}
+
+export function setQuizStatuses(quizStatuses, storageOverride = null) {
+  const storage = storageOverride || localStorage;
+
+  storage.setItem(QUIZ_STATUSES_NAME, JSON.stringify(quizStatuses));
+}
+
+export function setQuizStatus(quizID, status, storageOverride = null) {
+  const storage = storageOverride || localStorage;
+
+  // if (storageAvailable("localStorage")) {
+  let quizStatuses = getQuizStatuses(storage);
+
+  // If it doesn't exist yet, then initialize it
+  if (!quizStatuses) {
+    quizStatuses = {};
+  }
+
+  // Set the quiz status
+  quizStatuses[quizID] = {
+    status,
+    dateUpdated: Date.now(),
+  };
+
+  // Save it
+  setQuizStatuses(quizStatuses, storageOverride);
+
+  // Return true noting that we were able to save it
+  return true;
+}
---
+++ hebrew_quiz/src/components/Persistence/index.test.js
@@ -0,0 +1,65 @@
+import { getQuizStatuses, setQuizStatus, getQuizStatus } from "./index";
+
+// Setup a local storage mock
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = value.toString();
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+}
+
+test("setting quiz status", () => {
+  const storageMock = new LocalStorageMock();
+
+  // Add quiz statuses
+  setQuizStatus("vocab_1", true, storageMock);
+  setQuizStatus("vocab_2", false, storageMock);
+
+  // Make sure the latest entry is correct
+  const records = getQuizStatuses(storageMock);
+  expect(records["vocab_1"].status).toBe(true);
+  expect(records["vocab_1"].dateUpdated).toBeGreaterThan(1630429000000);
+
+  expect(records["vocab_2"].status).toEqual(false);
+  expect(records["vocab_2"].dateUpdated).toBeGreaterThan(1630429000000);
+
+  // Getting quiz status
+  const vocab_1 = getQuizStatus("vocab_1", storageMock);
+  expect(vocab_1.status).toBe(true);
+  expect(vocab_1.dateUpdated).toBeGreaterThan(1630429000000);
+
+  const vocab_2 = getQuizStatus("vocab_2", storageMock);
+  expect(vocab_2.status).toBe(false);
+  expect(vocab_2.dateUpdated).toBeGreaterThan(1630429000000);
+});
+
+test("get quiz status", () => {
+  const storageMock = new LocalStorageMock();
+
+  // Add quiz statuses
+  setQuizStatus("vocab_1", true, storageMock);
+  setQuizStatus("vocab_2", false, storageMock);
+
+  // Make sure the latest entry is correct
+  const records = getQuizStatuses(storageMock);
+  expect(records["vocab_1"].status).toBe(true);
+  expect(records["vocab_1"].dateUpdated).toBeGreaterThan(1630429000000);
+
+  expect(records["vocab_2"].status).toEqual(false);
+  expect(records["vocab_2"].dateUpdated).toBeGreaterThan(1630429000000);
+});

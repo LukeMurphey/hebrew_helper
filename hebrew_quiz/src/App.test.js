8e2d8265c028cfc678aadce6fd6e45843ceb10a1
Luke Murphey <Luke.Murphey@gmail.com>
2021-09-01 20:54:08 -0700

    Fixing the app test

--- hebrew_quiz/src/App.test.js
+++ hebrew_quiz/src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const header = screen.getByText(
+    /Hebrew Quizzes for Learning Biblical Hebrew/i
+  );
+  expect(header).toBeInTheDocument();
 });

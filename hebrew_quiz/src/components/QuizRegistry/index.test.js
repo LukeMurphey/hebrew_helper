f0cd5dd8823139e941e2d7fb4fb8d8371a31b267
Luke Murphey <Luke.Murphey@gmail.com>
2021-11-23 22:50:14 -0800

    Fixing check for quiz consistency

--- hebrew_quiz/src/components/QuizRegistry/index.test.js
+++ hebrew_quiz/src/components/QuizRegistry/index.test.js
@@ -1,29 +1,11 @@
 import QuizRegistry from "./index";
 
-const checkQuiz = (question) => {
-    const pathIsArray = Array.isArray(question.path);
-    const renderIsArray = Array.isArray(question.render);
-
-    // Neither are an array
-    if(!pathIsArray && !renderIsArray) {
-        return true;
-    }
-
-    // Path is an array. but render is not
-    if(pathIsArray && !renderIsArray) {
+const checkQuiz = (quizSet) => {
+    for (const [index, quiz] of quizSet.quizzes.entries()) {
+      if(quiz.path === undefined || quiz.render === undefined) {
         return false;
+      }
     }
-
-    // Render is an array. but path is not
-    if(!pathIsArray && renderIsArray) {
-        return false;
-    }
-
-    // Check the lengths
-    if(question.path.length !== question.render.length) {
-        return false;
-    }
-  
     // Everything checks out
     return true;
   };
@@ -32,7 +14,7 @@ const checkMultiStageQuizzes = (questionSet) => {
   const errors = questionSet.filter((question, i) => {
     if (!checkQuiz(question)) {
       console.error(
-        `Quiz ${i} is not well-formed; it has ${question.path.length} items in the path, but ${question.render.length} render functions`
+        `Quiz ${i} is not well-formed`
       );
       return true;
     }
